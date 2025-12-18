875ff335b87ecd6af0e6c16c87a33b754e9658a1
Harshini-Ananthan <harshiniananthan@protonmail.com>
2025-12-18 20:01:55 +0530

    Changed API BASE URL

--- src/api.js
+++ src/api.js
@@ -1,3 +1,5 @@
-const API_BASE_URL = "http://localhost:3000"; // Change this to "https://femine-backend.onrender.com" for production
+//const API_BASE_URL = "http://localhost:3000"; // Change this to "https://femine-backend.onrender.com" for production
+
+const API_BASE_URL = "https://femine-backend.onrender.com";
 
 export default API_BASE_URL;
